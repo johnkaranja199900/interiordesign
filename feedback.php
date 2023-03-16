88493250b5de4acb145cb1b538e35d08be2eee21
johnkaranja199900 <109080321+johnkaranja199900@users.noreply.github.com>
2023-03-16 18:47:31 +0300

    a

--- feedback.php
+++ feedback.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>EMIRATES|Design</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="F" name="keywords">
+    <meta content="wario" name="keywords">
     <meta content="s" name="description">
 
    
